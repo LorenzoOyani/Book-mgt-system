4ef1bac927eade4a3625424b5431ba82f5bcc5bc
Jerumeh Lawrence <JerumehLawrence@gmail.com>
2024-07-24 20:24:51 +0100

    refactor

--- src/main/java/com/example/crudapplication/book/Domain/CreateBookRequest.java
+++ src/main/java/com/example/crudapplication/book/Domain/CreateBookRequest.java
@@ -10,10 +10,10 @@ import java.math.BigDecimal;
 @Data
 public class CreateBookRequest {
     private BookId id;
-  private  Isbn isbn;
-  private  String bookName;
-  private  String fullName;
-  private  Integer stock;
-   private  BigDecimal price;
-   private AuthorId authorId;
+    private Isbn isbn;
+    private String bookName;
+    private String fullName;
+    private Integer stock;
+    private BigDecimal price;
+    private AuthorId authorId;
 };
